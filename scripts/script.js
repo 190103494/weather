beb351adcb77ff30eadb99cfc86e50c7ddd95bc7
190103494 <63901134+190103494@users.noreply.github.com>
2023-04-07 18:54:15 +0600

    feat: Make a website dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,9 @@
 //https://api.openweathermap.org/data/2.5/onecall?lat=48.785835&lon=-122.40642&appid=32304bca70420a2d98a8d0fc2416a813&units=metric
 // console.log("hw")
 let lat = 48.785835
-    // 76.90134
+// 76.90134
 let lon = -122.40642
-    // 43.248622
+// 43.248622
 const API_KEY = "32304bca70420a2d98a8d0fc2416a813"
 
 $( document ).ready(function() {
@@ -12,9 +12,48 @@ $( document ).ready(function() {
 });
 function renderContent(content){
     console.log(content)
-    document.querySelector("#currentTemp").innerText = Math.floor(content.current.temp) + "°"
+    //Main Block
+    document.querySelector("#currentTemp").innerText = Math.round(content.current.temp) + "°"
     document.querySelector("#city").innerText = content.timezone
-    document.querySelector("#currentDate").innerText = new Date(content.current.dt * 1000);
+    document.querySelector("#currentDate").innerText = new Date(content.current.dt * 1000).toLocaleDateString();
+    document.querySelector("#pressure").innerText = content.current.pressure
+    document.querySelector('#windSpeed').innerText = content.current.wind_speed
+    document.querySelector('#humidity').innerText = content.current.humidity
+    document.querySelector('#cloudness').innerText = content.current.clouds
+    document.querySelector('#currentState').innerText = content.current.weather[0].main
+    // document.querySelector('#currentIcon').src = setIcon(content.current.weather[0].icon)
+    //ChangeIcon
+    const iconLocation = content.current.weather[0].icon
+    document.querySelector('#currentIcon').src = `https://openweathermap.org/img/wn/${iconLocation}.png`
+
+
+    //Block1
+    const iconLocation1 = content.daily[0].weather[0].icon
+    document.querySelector('#firstIcon').src = `https://openweathermap.org/img/wn/${iconLocation1}.png`
+    document.querySelector('#firstTemp').innerText = Math.round(content.daily[0].temp.day) + "°"
+    document.querySelector('#firstState').innerText = content.daily[0].weather[0].main
+    // document.querySelector('#firstIcon').src = setIcon(content.daily[0].weather[0].icon)
+
+
+    //Block2
+    const iconLocation2 = content.daily[1].weather[0].icon
+    document.querySelector('#secondIcon').src = `https://openweathermap.org/img/wn/${iconLocation2}.png`
+    document.querySelector('#secondTemp').innerText = Math.round(content.daily[1].temp.day) + "°"
+    document.querySelector('#secondState').innerText = content.daily[1].weather[0].main
+
+    //Block3
+    const iconLocation3 = content.daily[2].weather[0].icon
+    document.querySelector('#thirdIcon').src = `https://openweathermap.org/img/wn/${iconLocation3}.png`
+    document.querySelector('#thirdTemp').innerText = Math.round(content.daily[2].temp.day) + "°"
+    document.querySelector('#thirdState').innerText = content.daily[2].weather[0].main
+
+    //Block4
+    const iconLocation4 = content.daily[3].weather[0].icon
+    document.querySelector('#forthIcon').src = `https://openweathermap.org/img/wn/${iconLocation4}.png`
+    document.querySelector('#forthTemp').innerText = Math.round(content.daily[3].temp.day) + "°"
+    document.querySelector('#forthState').innerText = content.daily[3].weather[0].main
+
+
 }
 function getWeather(){
     $.ajax({
@@ -71,3 +110,12 @@ function setCoordsLs(){
     lat = coords.lat
     lon = coords.lon
 }
+
+// function setIcon(code){
+//     const icons = {
+//         '04n' : '',
+//         '10d': ''
+//     }
+//     return icons[code]
+// }
+
